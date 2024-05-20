818aaa20dfe77b7631155d517cfe8b6fa138205f
Viktor Peredera <vitia2282@gmail.com>
2024-05-20 16:42:27 +0300

    feat: add dev minting form

--- src/config/networks/index.ts
+++ src/config/networks/index.ts
@@ -6,11 +6,6 @@ export const networks: Array<{
   logo: string;
 }> = [
   {
-    chainId: DexChainId.CALLISTO,
-    name: "Callisto",
-    logo: "/chains/callisto.svg",
-  },
-  {
     chainId: DexChainId.SEPOLIA,
     name: "Sepolia",
     logo: "/chains/sepolia.svg",

d438a9af9bdb11b6dd0436ce8fae5a714f005971
Viktor Peredera <vitia2282@gmail.com>
2024-06-18 20:33:48 +0300

    feat: add new tokens

--- src/config/connectors/keystore/connector.ts
+++ src/config/connectors/keystore/connector.ts
@@ -95,7 +95,7 @@ export function keystore({ pk }: KeystoreConnectorParameters) {
             webSocket("wss://eth-sepolia.g.alchemy.com/v2/kvidqVpyVu4aivBEb55XXIzCHDqMm7CO"),
             http("https://sepolia.infura.io/v3/6689c099b8d542589b1842e30dbc2027"),
             http("https://eth-sepolia.g.alchemy.com/v2/kvidqVpyVu4aivBEb55XXIzCHDqMm7CO"),
-            // http("https://rpc.ankr.com/eth_sepolia"),
+            http("https://rpc.ankr.com/eth_sepolia"),
             http(),
           ]),
         });
@@ -115,7 +115,7 @@ export function keystore({ pk }: KeystoreConnectorParameters) {
             webSocket("wss://eth-sepolia.g.alchemy.com/v2/kvidqVpyVu4aivBEb55XXIzCHDqMm7CO"),
             http("https://sepolia.infura.io/v3/6689c099b8d542589b1842e30dbc2027"),
             http("https://eth-sepolia.g.alchemy.com/v2/kvidqVpyVu4aivBEb55XXIzCHDqMm7CO"),
-            // http("https://rpc.ankr.com/eth_sepolia"),
+            http("https://rpc.ankr.com/eth_sepolia"),
             http(),
           ]),
         });
